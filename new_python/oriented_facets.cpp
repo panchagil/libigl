4b997f65a6335ccc79f38a5a5a09e850c8b78da4
Francis Williams <francis@fwilliams.info>
2018-08-02 22:43:52 -0400

    Bind all_edges, oriented_facets, and edges.

---
+++ new_python/oriented_facets.cpp
@@ -0,0 +1,32 @@
+#include <Eigen/Core>
+#include <numpyeigen_utils.h>
+#include <numpyeigen_typedefs.h>
+#include <igl/oriented_facets.h>
+#include <pybind11/pybind11.h>
+#include <exception>
+
+npe_function("oriented_facets")
+npe_arg("f", "type_i32", "type_i64")
+npe_default_arg("dtype", "pybind11::object", "pybind11::dtype(\"int32\")")
+npe_begin_code()
+
+npe::Map_f F((npe::Scalar_f*)f.data(), f.shape()[0], f.shape()[1]);
+
+switch(pybind11::dtype::from_args(dtype).type()) {
+  case numpyeigen::detail::NumpyTypeChar::char_i32:
+  {
+    Eigen::Matrix<std::int32_t, Eigen::Dynamic, Eigen::Dynamic> E;
+    igl::oriented_facets(F, E);
+    return NPE_MOVE_DENSE(E);
+  }
+  case numpyeigen::detail::NumpyTypeChar::char_i64:
+  {
+    Eigen::Matrix<std::int64_t, Eigen::Dynamic, Eigen::Dynamic> E;
+    igl::oriented_facets(F, E);
+    return NPE_MOVE_DENSE(E);
+  }
+  default:
+    throw std::runtime_error("Bad dtype. Only int32 and int64 are supported.");
+}
+
+npe_end_code()
