4b1bfe03dfe1ac81d66edf3930bb677086cd3604
Sebastian Koch <s.koch@phusion.eu>
2018-08-17 16:11:51 -0400

    Add jet function

---
+++ new_python/src/jet.cpp
@@ -0,0 +1,23 @@
+//#include <tuple>
+//#include <Eigen/Core>
+//#include <Eigen/Sparse>
+#include <npe.h>
+//#include <pybind11/pybind11.h>
+#include <igl/jet.h>
+
+
+npe_function(jet)
+npe_arg(z, dense_f64, dense_f32)
+npe_arg(normalize, bool)
+
+npe_begin_code()
+using namespace std;
+
+Eigen::Matrix<std::double_t, Eigen::Dynamic, Eigen::Dynamic> c;
+igl::jet(z, normalize, c);
+return NPE_MOVE_DENSE(c);
+
+npe_end_code()
+
+
+
