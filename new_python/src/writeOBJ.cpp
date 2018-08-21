c814c7cd9d218aa3f579bc401f85ee5d8b518a8c
Francis Williams <francis@fwilliams.info>
2018-08-20 20:56:12 -0400

    default arguments for writeOBJ

--- new_python/src/writeOBJ.cpp
+++ new_python/src/writeOBJ.cpp
@@ -4,15 +4,17 @@
 #include <npe.h>
 #include <igl/writeOBJ.h>
 
+
 npe_function(writeOBJ)
 npe_arg(str, std::string)
-npe_arg(v, dense_f64, dense_f32)
-npe_arg(f, dense_i64, dense_i32)
+npe_default_arg(v, dense_f64, dense_f32, pybind11::array_t<double>())
+npe_default_arg(f, dense_i64, dense_i32, pybind11::array_t<int>())
 
 npe_begin_code()
 using namespace std;
 
 bool ret = igl::writeOBJ(str, v, f);
+
 return ret;
 
 npe_end_code()
